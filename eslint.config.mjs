753880d784f48a91d5e76f3cc11921354c2e0aeb
shmaizer <valerashmaizer@gmail.com>
2024-11-21 15:53:34 +0300

    add styleint,i18n plugin for eslint, fix all style

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,22 +8,13 @@ import pluginReact from "eslint-plugin-react";
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    ignores: [
+      'node_module/**'
+    ],
     rules: {
       "react/jsx-indent": [2, 4],
       "react/jsx-indent-props": [2, 4],
-      "indent": [2, 4],
-      'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
-      'import/no-unresolved': 'off',
-      'import/prefer-default-export': 'off',
-      'no-unused-vars': 'warn',
-      'react/require-default-props': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react/jsx-props-no-spreading': 'warn',
-      'react/function-component-definition': 'off',
-      'no-shadow': 'off',
-      'import/extensions': 'off',
-      'import/no-extraneous-dependencies': 'off',
-      'no-underscore-dangle': 'off',
+      "indent": [2, 4]
     }
   },
   { languageOptions: { globals: globals.browser } },
